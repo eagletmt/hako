3f3db4ba6f35dfcbe808692fcc3234a50327663d
Kohei Suzuki <kohei-suzuki@cookpad.com>
2015-12-03 16:54:54 +0900

    Load default.yml if present

--- lib/hako/application.rb
+++ lib/hako/application.rb
@@ -8,7 +8,15 @@ module Hako
       path = Pathname.new(yaml_path)
       @id = path.basename.sub_ext('').to_s
       @root_path = path.parent
-      @yaml = YAML.load_file(yaml_path)
+      @yaml = load_default_yaml(@root_path).merge(YAML.load_file(yaml_path))
+    end
+
+    private
+
+    def load_default_yaml(root_path)
+      YAML.load_file(root_path.join('default.yml').to_s)
+    rescue Errno::ENOENT
+      {}
     end
   end
 end
