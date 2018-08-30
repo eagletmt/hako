3a304096589ec1781331fca60cfbe1e33cb969de
Kohei Suzuki <eagletmt@gmail.com>
2018-08-30 18:05:24 +0900

    BREAKING: Drop YAML support
    hako v3 supports Jsonnet format only.

--- lib/hako/application.rb
+++ lib/hako/application.rb
@@ -2,7 +2,6 @@
 
 require 'hako/error'
 require 'hako/jsonnet_loader'
-require 'hako/yaml_loader'
 require 'pathname'
 
 module Hako
@@ -19,15 +18,7 @@ module Hako
       path = Pathname.new(definition_path)
       @id = path.basename.sub_ext('').to_s
       @root_path = path.parent
-      @definition =
-        case path.extname
-        when '.yml', '.yaml'
-          YamlLoader.new.load(path)
-        when '.jsonnet', '.json'
-          JsonnetLoader.new(self, expand_variables: expand_variables, ask_keys: ask_keys).load(path)
-        else
-          raise Error.new("Unknown extension: #{path}")
-        end
+      @definition = JsonnetLoader.new(self, expand_variables: expand_variables, ask_keys: ask_keys).load(path)
     end
   end
 end
